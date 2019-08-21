de37e650143b5b51752438e83a627309e96ea8ab
Anton Chernov <lirrleadolf5@gmail.com>
2019-08-21 15:01:41 +0300

    server starts and redirects

--- Server/server.js
+++ Server/server.js
@@ -14,41 +14,25 @@ app.use(
     extended: false
   })
 );
-
-function find(collec, query, callback) {
-  mongoose.connection.db.collection(collec, function(err, collection) {
-    collection.find(query).toArray(callback);
-  });
-}
-
-app.use("/api", api);
-
+app.use("/ideas", api);
 mongoose
   .connect("mongodb://127.0.0.1/ideas")
   .then(() => console.log("Connected to MongoDB"))
   .catch(error => console.error(`Couldn't connect to MongoDB ${error}`));
-
 app.use(bodyParser.json());
-
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
-let ideas;
-idea.find((err, ideass) => {
-  ideas = ideass;
-  console.log(ideass);
-});
 
-//app.use(express.static(path.join(__dirname, "../dist/english-flashcards")));
-
-app.get("/", (req, res) => {
-  res.send(ideas);
-});
 const port = process.env.PORT || "3000";
 app.set("port", port);
 
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Running on localhost ${port}`));
+
+app.get("/", (req, res) => {
+  res.redirect("/ideas");
+});
